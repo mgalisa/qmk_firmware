edb9aec2a49a548b436e1db59e82a57415257c83
Matt Galisa <matt.galisa@gmail.com>
2019-06-04 22:33:19 -0500

    New raise/lower layers

--- keyboards/planck/keymaps/mgalisa/keymap.c
+++ keyboards/planck/keymaps/mgalisa/keymap.c
@@ -126,11 +126,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
-[_RAISE] = LAYOUT_planck_grid(
+/* [_RAISE] = LAYOUT_planck_grid(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+), */
+
+/* Raise - new
+ * ,-----------------------------------------------------------------------------------.
+ * |   `  |  F1  |  F2  |  F3  |  F4  |      |   1  |   2  |   3  |   -  |   +  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Del  |  F5  |  F6  |  F7  |  F8  |      |   4  |   5  |   6  |   +  |   /  |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |  F9  |  F10 |  F11 |  F12 |      |   7  |   8  |   9  |Vol - |Vol + |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      0      |      | Home |Pg Dn |Pg Up | End  |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_RAISE] = LAYOUT_planck_grid(
+    KC_GRV,  KC_F1,   KC_F2,    KC_F3,    KC_F4,   _______,  KC_1,  KC_2, KC_3,  KC_MINS, KC_PLUS, KC_BSPC,
+    KC_DEL,  KC_F5,   KC_F6,    KC_F7,    KC_F8,   _______,  KC_4,  KC_5, KC_6,  KC_PLUS, KC_SLSH, KC_BSLS,
+    _______, KC_F9,   KC_F10,   KC_F11,   KC_F12,  _______,  KC_7,  KC_8, KC_9,  KC_PGUP, KC_VOLU, _______,
+    _______, _______, _______, _______, _______, KC_0, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
 /* Plover layer (http://opensteno.org)
--- keyboards/planck/keymaps/mgalisa/rules.mk
+++ keyboards/planck/keymaps/mgalisa/rules.mk
@@ -1,1 +1,4 @@
 SRC += muse.c
+EXTRAKEY_ENABLE = yes
+NKRO_ENABLE = yes
+# UNICODEMAP_ENABLE = yes
