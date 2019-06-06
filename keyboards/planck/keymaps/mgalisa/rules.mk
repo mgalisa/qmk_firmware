6b3f4e0892378cad56dfbb00db4550bc3848ebf8
Matt Galisa <matt.galisa@gmail.com>
2019-06-05 19:57:46 -0500

    Update rules

--- keyboards/planck/keymaps/mgalisa/rules.mk
+++ keyboards/planck/keymaps/mgalisa/rules.mk
@@ -1,4 +1,6 @@
 SRC += muse.c
 EXTRAKEY_ENABLE = yes
-NKRO_ENABLE = yes
-# UNICODEMAP_ENABLE = yes
+# RGBLIGHT_ENABLE = yes
+## I bought the WS2812s...
+## just need the main branch to be updated.
+# Uncomment to make build fail.
